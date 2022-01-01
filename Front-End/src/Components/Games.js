2d03741e86d5b2a5d207e754dc0130ad01e516c7
muirisc <91299876+muirisc@users.noreply.github.com>
2022-01-01 01:38:02 -0500

    More Cards

--- Front-End/src/Components/Games.js
+++ Front-End/src/Components/Games.js
@@ -1,11 +1,13 @@
 function Games() {
   return (
-    <>
-      <h3> It Takes Two </h3>
-      {/* <img/> */}
-      <p> Rating: T </p>
-      <p> Where to get: </p>
-    </>
+    <div className="gamebox">
+    <ol className="game">
+      <h3 className="gametitle"> It Takes Two </h3>
+      <img className="gameimg" alt="boxart for game" src="https://upload.wikimedia.org/wikipedia/en/a/aa/It_Takes_Two_cover_art.png"/>
+      <p className="gamerating"> Rating: T </p>
+      <p className="gameget"> Where to get: </p>
+    </ol>
+    </div>
   );
 }
 
--- Front-End/src/Components/MainPageContent.js
+++ Front-End/src/Components/MainPageContent.js
@@ -1,17 +1,17 @@
 function MainPageContent(){
 
 
-
-
   return(
-    <>
+
    <main role="main" id="main" tabindex="0">
     <section class="banner">
-      <h1 className="intro">Welcome to Clickplay!</h1>
+      <h1 className="intro">Welcome to Click & Play!</h1>
       <p className="introMessage"> These are the games you are searching for! </p>
+      <p className="introMessage">Enter a game in the search bar or filter through games via the genre filter option! </p> 
+       <p className="introMessage"> Find out how the game was received by criticsX, a quick description, and where you can play and purchase the game! </p>
     </section>
     </main>
-    </>
+    
 
   )
 }
